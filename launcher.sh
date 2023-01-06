29998a68aee269f4cd1c22a69dd48d3f582c68b8
aluvare <luvare@gmail.com>
2023-01-06 21:08:09 +0100

    some improvements in go-any-proxy

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,7 @@
-sed -i "s/PROXYSTRING/${PROXY_STRING}/g" /etc/supervisor/conf.d/redirector.conf
+sed -i "s#PROXYSTRING#${PROXY_STRING}#g" /etc/supervisor/conf.d/redirector.conf
+if [[ "$FORCE_SSL" != "" ]];then
+	sed -i "s/ -x=0/ -x=0 -allssl=1/g" /etc/supervisor/conf.d/redirector.conf
+fi
 if [ -f /opt/redirector/iptables ];then
 	bash /opt/redirector/iptables
 fi
@@ -7,9 +10,6 @@ if [ -f /etc/wireguard/wg.conf ];then
 else
 	echo "You cannot work with this router if you dont have /etc/wireguard/wg.conf"
 	exit 1
-fi
-if [[ "$FORCE_SSL" != "" ]];then
-	sed -i "s/ -x=0/ -x=0 -allssl=1/g" /etc/supervisor/conf.d/redirector.conf
 fi
 ls /scripts/init-scripts|while read i;do bash /scripts/init-scripts/$i;done 
 supervisord
